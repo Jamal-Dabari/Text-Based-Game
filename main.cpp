f191ff856e31e3c3a391bac7020e80b49c83f4ac
Jamal-Dabari <dabari.jamal@gmail.com>
2024-05-07 01:45:31 -0400

    game story update

--- main.cpp
+++ main.cpp
@@ -29,5 +29,25 @@ int main() {
   }
 
   std::cout << "Welcome " << characterName << std::endl;
+
+  std::cout << "\nYou wake up alone you look around you and you see nothing "
+               "but woods you start to look around"
+            << " but you see no one. \nYou are Alone\nYou decide to walk "
+               "around and see if you can find anyone\nas you"
+            << " stumble around you see in front of you a bear" << std::endl;
+
+  std::cout << "What do you do?\n1.Attack\n2.Run away" << std::endl;
+  std::cin >> characterChoice;
+
+  switch (characterChoice) {
+  case 1:
+    std::cout << "You try and attack but die bravely" << std::endl;
+    break;
+  case 2:
+    std::cout << "You escape" << std::endl;
+    break;
+  default:
+    std::cout << "Pick again" << std::endl;
+  }
   return 0;
 }
